8e0b39ba00a5219c1b7271b5c49a765c9b7e0cd0
aomina-web3 <scm.analysis@gmail.com>
2023-05-07 02:45:00 +0900

    6th commit

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from PIL import Image
 img = Image.open('butsuryudao_logo.png')
 st.image(img, caption='Butsuryu-DAO', use_column_width=True)
 
-st.title('物流DAOアプリ　プロトタイプサイト')
+st.title('物流DAOアプリ')
+st.title('プロトタイプサイト')
 
 st.write('物流DAOのプロトタイプアプリを試す事ができるサイトです。')
-st.write('各アプリを確認するには左のサイドバーのメニューからアプリを選択してください。')
+st.write('各アプリを確認するには左のサイドバーのメニュー">"からアプリを選択してください。')
