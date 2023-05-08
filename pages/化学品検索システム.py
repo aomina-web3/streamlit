fa7d242e5f5c44c03501eb839ec9c2aa910f5cf4
aomina-web3 <scm.analysis@gmail.com>
2023-05-09 01:44:00 +0900

    7th commit

--- pages/化学品検索システム.py
+++ pages/化学品検索システム.py
@@ -5,4 +5,18 @@ from PIL import Image
 
 
 st.title('化学品検索システム プロトタイプ')
-st.write('現在、作成中です。。')
+
+
+keyword = st.text_input('(キーワード検索)', 'Input something...')
+st.write('The current searching is', keyword)
+
+options1 = st.multiselect(
+    label="検索項目",
+    options=['CAS RN', '化審法番号', '安衛法番号', 'EC番号', '国連番号', 'HSコード', 'CHRIP_ID', '日化辞番号'],
+    default=('CAS RN', '化審法番号')
+    )
+
+option2 = st.selectbox(
+    '検索方法',
+    ('完全一致', '部分一致', '前方一致', '後方一致'))
+st.write('You selected:', option2)
--- pages/物流DAO_KPI.py
+++ pages/物流DAO_KPI.py
@@ -8,11 +8,15 @@ st.title('物流DAO KPI')
 
 st.write('こちらは物流DAOのKPIを確認できるサイトです。')
 
+col1, col2= st.columns(2)
+col1.metric("参加者", "41", "12")
+col2.metric("物流note記事数", "28", "3")
 
 df = pd.DataFrame({
     '参加者':[1,7,16,20,29,41],
     '物流note記事数':[6,12,17,21,25,28]},
-    index = pd.Index(["20221204","20230101","20230201","20230327","20230507","20230930"], name='日付')
+    index = pd.Index(["2212","2301","2302","2303","2304","2305"], name='日付')
 )
 st.write(df)
 st.line_chart(df)
+
